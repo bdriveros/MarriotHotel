8e8a3e8c8159bf1c2db08b830962eb296d928223
bdriveros <brayandrj9@gmail.com>
2022-08-12 23:08:54 -0500

    Forms

--- forms.py
+++ forms.py
@@ -2,13 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
-class Producto(FlaskForm):
-    codigo = StringField('id', validators=[DataRequired()])
-    nombre = StringField('Nombre', validators=[DataRequired()])
-    precio = StringField('Precio', validators=[DataRequired()])
-    cantidad = StringField('Cantidad', validators=[DataRequired()])
-    enviar = SubmitField('Agregar Producto')
-
 class Usuarios(FlaskForm):
     Id = StringField('Id', validators=[DataRequired()])
     Nombres = StringField('Nombres', validators=[DataRequired()])
